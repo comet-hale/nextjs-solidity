84e18475cdb4c33292b091e1c9c05ec79a98baa6
dev <dev@devs-MacBook-Pro.local>
2022-02-14 19:33:31 -0600

    Target network, CourseCard

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useWeb3 } from "@components/providers";
 import { Hero } from "@components/ui/common";
-import { CourseList } from "@components/ui/course";
+import { CourseList, CourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
 
@@ -10,7 +10,9 @@ export default function Home({ courses }) {
   return (
     <>
       <Hero />
-      <CourseList courses={courses} />
+      <CourseList courses={courses}>
+        {(course) => <CourseCard key={course.id} course={course} />}
+      </CourseList>
     </>
   );
 }
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,4 +1,4 @@
-import { CourseList } from "@components/ui/course";
+import { CourseCard, CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
 import { WalletBar } from "@components/ui/web3";
@@ -11,9 +11,18 @@ export default function Marketplace({ courses }) {
   return (
     <>
       <div className="py-4">
-        <WalletBar address={account.data} network={network.data} />
+        <WalletBar
+          address={account.data}
+          network={{
+            data: network.data,
+            target: network.target,
+            isSupported: network.isSupported,
+          }}
+        />
       </div>
-      <CourseList courses={courses} />
+      <CourseList courses={courses}>
+        {(course) => <CourseCard key={course.id} course={course} />}
+      </CourseList>
     </>
   );
 }
