02c544051ef04f0c4a21dad9131e93311b4b793e
dev <dev@devs-MacBook-Pro.local>
2022-02-25 09:01:08 -0600

    182: Load contract

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -7,6 +7,7 @@ const {
 } = require("react");
 
 import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "@utils/loadContract";
 import Web3 from "web3";
 import { setupHooks } from "./hooks/setupHooks";
 
@@ -26,10 +27,11 @@ export default function Web3Provider({ children }) {
       const provider = await detectEthereumProvider();
       if (provider) {
         const web3 = new Web3(provider);
+        const contract = loadContract("CourseMarketplace");
         setWeb3Api({
           provider,
           web3,
-          contract: null,
+          contract,
           isLoading: false,
           hooks: setupHooks(web3, provider),
         });
