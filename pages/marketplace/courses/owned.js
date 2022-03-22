29f2b77ebdbcc06ed3a185659f152a444546bc9f
dev <dev@devs-MacBook-Pro.local>
2022-03-22 02:59:21 -0500

    Add web3 state

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,9 +1,12 @@
+import { useOwnedCourses } from "@components/hooks/web3";
 import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 
 export default function OwnedCourses() {
+  const { ownedCourses } = useOwnedCourses();
+
   return (
     <>
       <div className="py-4">
