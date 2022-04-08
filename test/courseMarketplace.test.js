dad2b40fd5e4c438d9bccc3cb0b0dc9d3682718c
dev <dev@devs-MacBook-Pro-2.local>
2022-04-07 22:36:51 -0500

    Preparing test

---
+++ test/courseMarketplace.test.js
@@ -0,0 +1,19 @@
+const CourseMarketplace = artifacts.require("CourseMarketplace");
+
+contract("CourseMarketplace", (accounts) => {
+  let _contract = null;
+  let contractOwner = null;
+  let buyer = null;
+
+  before(async () => {
+    _contract = await CourseMarketplace.deployed();
+    contractOwner = accounts[0];
+    buyer = accounts[1];
+  });
+
+  describe("Purchase the new course", () => {
+    it("should resolve into true value", () => {
+      assert(true, "Value is NOT true");
+    });
+  });
+});
